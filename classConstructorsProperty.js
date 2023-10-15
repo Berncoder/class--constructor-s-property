31eb8931eb0f2fb3c091fc8227c74b34871f789a
Bernadette <bernadettemetran35@gmail.com>
2023-10-14 22:11:39 -0700

    first commit

---
+++ classConstructorsProperty.js
@@ -0,0 +1,33 @@
+//using class instance as another class' constructor's property
+
+class StationaryBike {
+    constructor(position, gears) {
+        this.position = position
+        this.gears = gears
+    }
+}
+
+class Treadmill {
+    constructor(position, modes) {
+        this.position = position
+        this.modes = modes
+    }
+}
+
+class Gym {
+    constructor(openHrs, stationaryBikePos, treadmillPos) {
+        this.openHrs = openHrs
+        this.stationaryBike = new StationaryBike(stationaryBikePos, 8)
+        this.treadmill = new Treadmill(treadmillPos, 5)
+    }
+}
+
+var boxingGym = new Gym("7-22", "right corner", "left corner")
+
+console.log(boxingGym.openHrs)
+console.log(boxingGym.stationaryBike)
+console.log(boxingGym.treadmill)
+
+/* console output: 7-22
+StationaryBike { position: 'right corner', gears: 8 }
+Treadmill { position: 'left corner', modes: 5 } */
